c6384ced9a04a88b571a18b82fa1f3019a0cc7e9
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-05-30 15:49:53 +0200

    add styling

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,41 @@
 <template>
-    <div class="notes">
-        <h1>Notes</h1>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new Note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New Note</button>
+        </div>
+      </div>
     </div>
+
+    <div v-for="i in 3" class="card mb-4">
+      <header class="card-header">
+        <p class="card-header-title">Component</p>
+        <button class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sed
+          expedita voluptatibus assumenda facilis corrupti sint dolorum quam
+          autem. Ad at beatae, non dolor illum unde quae pariatur debitis
+          explicabo.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
