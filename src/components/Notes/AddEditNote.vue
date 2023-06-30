82e9d65906156edc91ed40ffcc6e34a1f024d052
Miguel Fernandez <miguel.fernandez@docplanner.com>
2023-06-30 09:07:25 +0200

    extract

---
+++ src/components/Notes/AddEditNote.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new Note" ref="newNoteRef" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+            <slot name="buttons"></slot>
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,40 +1,35 @@
 <template>
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <label class="label">Message</label>
-        <div class="control">
-          <textarea v-model="newNote" class="textarea" placeholder="Add a new Note" ref="newNoteRef" />
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button :disabled="!newNote" @click="addNote" class="button is-link has-background-success">Add New Note</button>
-        </div>
-      </div>
-    </div>
+    <AddEditNote>
+      <template #buttons>
+        <button
+          :disabled="!newNote"
+          @click="addNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
 
     <Note v-for="note in storeNotes.notes" key="note.id" :note="note" />
-
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue";
+import AddEditNote from "@/components/Notes/AddEditNote.vue";
+import { useStoreNotes } from "@/stores/storeNotes";
 
-import { ref } from 'vue'
-import Note from '@/components/Notes/Note.vue'
-import { useStoreNotes } from '@/stores/storeNotes';
+const storeNotes = useStoreNotes();
 
-const storeNotes = useStoreNotes()
-
-const newNote = ref('')
-const newNoteRef = ref(null)
+const newNote = ref("");
+const newNoteRef = ref(null);
 
 const addNote = () => {
-
-  storeNotes.addNote(newNote.value)
-  newNote.value = ''
-  newNoteRef.value.focus()
-}
+  storeNotes.addNote(newNote.value);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
 </script>
